16fa5e31f564f717891a7a511575c6f5e2832f31
jamie <jamiedz999@gmail.com>
2025-11-25 17:19:25 +0000

    weixin-portal

--- s-pay-mall-web/src/main/java/com/jamie/Application.java
+++ s-pay-mall-web/src/main/java/com/jamie/Application.java
@@ -3,10 +3,16 @@ package com.jamie;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
 
-@SpringBootApplication
+
 @Configuration
+@SpringBootApplication(exclude = {
+        DataSourceAutoConfiguration.class,
+        DataSourceTransactionManagerAutoConfiguration.class
+})
 public class Application {
 
     public static void main(String[] args) {
